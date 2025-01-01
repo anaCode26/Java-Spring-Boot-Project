ef34986da6c8ad4e10a79ed8b72aac3d606703cf
Ana Gabriela <anagabrielaguz26@gmail.com>
2025-01-01 18:22:38 +0100

    convert into record the registrationDTO

--- restservice/src/main/java/com/example/restservice/security/dto/RegistrationDTO.java
+++ restservice/src/main/java/com/example/restservice/security/dto/RegistrationDTO.java
@@ -1,44 +1,7 @@
 package com.example.restservice.security.dto;
 
-public class RegistrationDTO {
-
-    private String email;
-    private String password;
-    private String name;
-    private String address;
-
-    public RegistrationDTO() {}
-    public RegistrationDTO(String email, String password, String name, String address) {}
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-}
+public record RegistrationDTO(
+        String email,
+        String password,
+        String name,
+        String address) {}
