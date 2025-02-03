7458b8abcca0490c067ecdc20fbce1a8ab981d8a
Ana Gabriela <anagabrielaguz26@gmail.com>
2025-02-03 15:57:02 +0100

    unnecessary files deleted..

--- testcypress/cypress/e2e/petEndpoint.cy.js
+++ testcypress/cypress/e2e/petEndpoint.cy.js
@@ -29,7 +29,7 @@ describe("Validate the pet methods", () => {
         });
     });
 
-    it.skip("POST: create_valid pet_succeeds", () => {
+    it("POST: create_valid pet_succeeds", () => {
         cy.request({
             method: "POST",
             url: "http://localhost:8080/api/pet",
@@ -72,7 +72,7 @@ describe("Validate the pet methods", () => {
         });
     });
 
-    it.skip("DELETE: deletes an existing pet", () => {
+    it("DELETE: deletes an existing pet", () => {
         cy.request({ method: "DELETE",
             url: "http://localhost:8080/api/pet/6"}).then(
             (response) => {
